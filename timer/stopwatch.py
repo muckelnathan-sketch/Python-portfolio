cc419a094a7a27a16e5fada500b2bfd4363ab30f
muckelnathan-sketch <muckelnathan@gmail.com>
2026-01-03 18:48:48 -0500

    created a timer and stopwatch application which i will create more user friendly in the days following
    implemented a applicated with the user able to decide to use it as a timer using input in seconds and a stopwatch having the system set a before time and then having the player be able to press enter into a input rentering the program and printing the time.time() with the start time, also cleaned up USDCONVERTER

---
+++ timer/stopwatch.py
@@ -0,0 +1,16 @@
+import time
+#create functions to be used for the application later
+def stopwatch():
+    #create a start time so that we can subtract the afterwards time when the player ends the stopwatch 
+    start = time.time()
+    input("press enter to stop stopwatch ") #when the user submits using the input its them pretty much pressing a button
+    print(f"{time.time() - start:.2f} seconds")
+def timer():
+    how_long = input("how many seconds to wait: ")
+    time.sleep(how_long) #sleep the amount of seconds  the user wishes, perhaps i could work in a system that finds how many minutes and seconds the user wants
+    print("done!")
+while True:
+    mode = input("stopwatch or timer: ")
+    if mode == 'stopwatch':stopwatch();break 
+    if mode == 'timer':break; timer();break #using semi colons for a cleaner look
+    #i think tmr i will begin to clean up the code , perhaps using match/case for simpler code
